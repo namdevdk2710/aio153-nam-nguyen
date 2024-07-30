7b05ab8b76d11e32c928f2492a573b455401a2d1
Nam Nguyen <namnguyen@Nams-MacBook-Pro.local>
2024-07-30 22:05:58 +0700

    feat: make exercise module 02 week 03

---
+++ module_02/week_03/exercise_02.py
@@ -0,0 +1,36 @@
+# P("Class"="On Time") = 14/20, P("Class"="Late") = 2/20, P("Class"="Very Late") = 3/20, P("Class"="Cancelled") = 1/20
+# Question 5 => A
+
+
+# P(Day=Weekday | "Class"="On Time") = 9/14
+# P(Season=Winter | "Class"="On Time") = 2/14
+# P(Fog=High | "Class"="On Time") = 4/14
+# P(Rain=Heavy | "Class"="On Time") = 2/14
+# P("Class"="On Time" | X) ∝ P(X | "Class"="On Time") × P("Class"="On Time") = ?
+# Question 6 => ?
+
+
+# P(Day=Weekday | "Class"=Late) = 2/4
+# P(Season=Winter | "Class"=Late) = 1/4
+# P(Fog=High | "Class"=Late) = 3/4
+# P(Rain=Heavy | "Class"=Late) = 2/4
+# P("Class"=Late | X) ∝ P(X | "Class"=Late) × P("Class"=Late) = ?
+# Question 7 =>
+
+
+# P(Day=Weekday | "Class"="Very Late") = 2/4
+# P(Season=Winter | "Class"="Very Late") = 1/4
+# P(Fog=High | "Class"="Very Late") = 3/4
+# P(Rain=Heavy | "Class"="Very Late") = 2/4
+# P("Class"="Very Late" | X) ∝ P(X | "Class"="Very Late") × P("Class"="Very Late") = ?
+# Question 8 => ?
+
+# P(Day=Weekday | "Class"="Cancelled") = 2/4
+# P(Season=Winter | "Class"="Cancelled") = 1/4
+# P(Fog=High | "Class"="Cancelled") = 3/4
+# P(Rain=Heavy | "Class"="Cancelled") = 2/4
+# P("Class"="Cancelled" | X) ∝ P(X | "Class"="Cancelled") × P("Class"="Cancelled") = ?
+# Question 9 => ?
+
+
+# Question 10 => ?
